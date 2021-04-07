8a1fd385c300fcfbccb91aab3317cd35b0d6a34c
adpp420 <richianand1990@gmail.com>
2021-04-07 22:53:27 +0530

    new trader activity was added

---
+++ app/src/main/java/com/example/aquafish/TraderActivity.java
@@ -0,0 +1,14 @@
+package com.example.aquafish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TraderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trader);
+    }
+}
